09eadbc48faf85665312ea8cd5ecb86ba5cfdaff
asafonov-ops <72823707+asafonov-ops@users.noreply.github.com>
2025-05-17 22:22:48 -0400

    Update immutable_dbb_clone.cmd
    Expiration of older version of DBBs implemented.
    Copying is done with 4 parallel processes

--- immutable_dbb_clone.cmd
+++ immutable_dbb_clone.cmd
@@ -2,6 +2,7 @@
 
 CMD=rclone                                                                                                      # Tested with rclone v1.68.1
 FLAGS="--s3-chunk-size 16M --s3-upload-cutoff 0  --disable ServerSideAcrossConfigs  --stats 1s --progress"      # For production deployment you may want to remove "--stats 1s --progress"
+# FLAGS="--s3-chunk-size 16M --s3-upload-cutoff 0  --disable ServerSideAcrossConfigs  " # For production deployment you may want to remove "--stats 1s --progress"
 SRC=COS_C2LAB                                                                                                   # rclone profile name, this profile must have at least read-only access to the
                                                                                                                 # vault where TSM writes DB backup
 DST=COS_C2LAB_RETENTION                                                                                         # rclone profile name, this profile must have read/write access to the target
@@ -9,17 +10,45 @@ DST=COS_C2LAB_RETENTION                                                         
 INCLUDE_DBV="--include *.DBV"                                                                                   # TSM naming convention for DBB files/objects
 SRC_BUCKET=tsminst1dbb                                                                                          # Bucket name in TSM configuration where DBB is stored
 DST_BUCKET=tsminst1dbbretday$(echo $(($(date +%s) / 86400))%4 + 1| bc)                                          # Target retention enabled vault name, every day the name alternated day1 day2 day3 day4 day1 ...
+# DST_BUCKET=tsminst1dbb1d                                              # Target retention enabled vault name, every day the name alternated day1 day2 day3 day4 day1 ...
 INSTANCE_ID=dbbackup-tsminst1-c4fb71884961ee11955b82aaed4dffed                                                  # Name selected by your TSM server instance, must match your actual identifier
 DBB_DEVCLASS=DBBCOS                                                                                             # TSM devclass name used for DB backup 'backup DB devclass=DBBCOS type=full'
+
+DBV_COUNT=$($CMD ls $DST:$DST_BUCKET --include *DBV | wc -l)
+
+if [[ $? -ne 0 ]]
+        then echo Failed to run $CMD ls $DST:$DST_BUCKET
+        exit -1
+fi
+
+if [[ $DBV_COUNT -gt 0 ]]
+        then
+                echo Deleting expired devconfig,volumehistory,enckey,DBV objects
+                $CMD ls $DST:$DST_BUCKET --include volhistory | awk '{ print $2}'  | $CMD delete --files-from - $DST:$DST_BUCKET > /dev/null 2>&1
+                if [[ $? -ne 0 ]]
+                        then
+                                echo Unable to delete $DST_BUCKET/$INSTANCE_ID/volhistory, InvalidRequestForLegalReasons: The object is protected
+                                echo Exiting
+                                exit 1
+                        else
+                                echo Deleting $DST:$DST_BUCKET/$INSTANCE_ID/volhistory: Deleted
+                                for i in devconfig enckey *.DBV ; do
+                                $CMD ls $DST:$DST_BUCKET --include $i | awk '{ print $2}'  | $CMD delete -v --files-from - $DST:$DST_BUCKET
+                                done
+                fi
+fi
+
 
 
 DBB_LATEST_SERIES=$($CMD cat $SRC:$SRC_BUCKET/$INSTANCE_ID/volhistory | egrep -A 1 -B 3 "Device Class Name:\s+$DBB_DEVCLASS" | grep "Backup Series:" | awk '{ print $3 }' | sort -u | tail -1)
 
 echo DBB latest series: $DBB_LATEST_SERIES
 
-for i in $(rclone cat $SRC:$SRC_BUCKET/$INSTANCE_ID/volhistory | egrep -A4 -B1 "Backup Series:\s+$DBB_LATEST_SERIES" | grep "Volume Name: " | awk -F '"' '{ print $2}' ) ;
-do echo Cloning $i ; $CMD copy $SRC:$i $DST:/$DST_BUCKET/$INSTANCE_ID/volumes/DBV/ $FLAGS;
- done
+# for i in $($CMD cat $SRC:$SRC_BUCKET/$INSTANCE_ID/volhistory | egrep -A4 -B1 "Backup Series:\s+$DBB_LATEST_SERIES" | egrep -A4 -B1 "Device Class Name:\s+$DBB_DEVCLASS" | grep "Volume Name: " | awk -F '"' '{ print $2}' ) ;
+# do echo Cloning $i ; $CMD copy $SRC:$i $DST:/$DST_BUCKET/$INSTANCE_ID/volumes/DBV/ $FLAGS;
+#  done
+echo Cloning DBV objects to $DST_BUCKET/$INSTANCE_ID/
+$CMD cat $SRC:$SRC_BUCKET/$INSTANCE_ID/volhistory | egrep -A4 -B1 "Backup Series:\s+$DBB_LATEST_SERIES" | egrep -B4 -A1 "Device Class Name:\s+$DBB_DEVCLASS" | grep "Volume Name: " | awk -F '"' '{ print $2}' | awk -F '/' '{ print $2"/"$3"/"$4"/"$5}' | $CMD copy $FLAGS --files-from -  COS_C2LAB:$SRC_BUCKET COS_C2LAB_RETENTION:$DST_BUCKET
 
 echo Cloning volhistory
 $CMD copy $SRC:$SRC_BUCKET/$INSTANCE_ID/volhistory $DST:/$DST_BUCKET/$INSTANCE_ID/volhistory.orig
@@ -35,9 +64,8 @@ $CMD cat  $DST:/$DST_BUCKET/$INSTANCE_ID/devconfig.orig/devconfig    | sed -e 's
 
 # Known Limitations:
 #
-# Expiration and cleanup of expired version of TSM DB backups is not yet implemented
 # Only uses a single COS Accesser (defined in rclone profile)
-# Copies one DBV object at a time, may limit performance
-# Clones only one last TSM DB Backup Series
+# Clones only one last TSM full DB Backup Series
+# Supports full TSM DBB only, no snapshots or incremental backups
 # No synchronization with the actual TSM "backup db" process, requires manual synchronization
 # Target vault retention should be set to 3 days (4 days rotation minus 1 day).
