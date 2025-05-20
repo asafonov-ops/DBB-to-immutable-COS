78e4914e661b5485e7bc209af522d04411a5a16e
asafonov-ops <72823707+asafonov-ops@users.noreply.github.com>
2025-05-20 10:13:36 -0400

    Update immutable_dbb_clone.cmd

--- immutable_dbb_clone.cmd
+++ immutable_dbb_clone.cmd
@@ -2,14 +2,15 @@
 
 CMD=rclone                                                                                                      # Tested with rclone v1.68.1
 FLAGS="--s3-chunk-size 16M --s3-upload-cutoff 0  --disable ServerSideAcrossConfigs  --stats 1s --progress"      # For production deployment you may want to remove "--stats 1s --progress"
-# FLAGS="--s3-chunk-size 16M --s3-upload-cutoff 0  --disable ServerSideAcrossConfigs  " # For production deployment you may want to remove "--stats 1s --progress"
+
 SRC=COS_C2LAB                                                                                                   # rclone profile name, this profile must have at least read-only access to the
                                                                                                                 # vault where TSM writes DB backup
 DST=COS_C2LAB_RETENTION                                                                                         # rclone profile name, this profile must have read/write access to the target
                                                                                                                 # retention enabled vault
+DBB_RETENTION_INTERVAL=4                                                                                        # Days to retain DBB clones bafore expiration. Note COS retention in managed separately
 INCLUDE_DBV="--include *.DBV"                                                                                   # TSM naming convention for DBB files/objects
 SRC_BUCKET=tsminst1dbb                                                                                          # Bucket name in TSM configuration where DBB is stored
-DST_BUCKET=tsminst1dbbretday$(echo $(($(date +%s) / 86400))%4 + 1| bc)                                          # Target retention enabled vault name, every day the name alternated day1 day2 day3 day4 day1 ...
+DST_BUCKET=tsminst1dbbretday$(echo $(($(date +%s) / 86400))%$DBB_RETENTION_INTERVAL + 1| bc)                                            # Target retention enabled vault name, every day the name alternated day1 day2 day3 day4 day1 ...
 # DST_BUCKET=tsminst1dbb1d                                              # Target retention enabled vault name, every day the name alternated day1 day2 day3 day4 day1 ...
 INSTANCE_ID=dbbackup-tsminst1-c4fb71884961ee11955b82aaed4dffed                                                  # Name selected by your TSM server instance, must match your actual identifier
 DBB_DEVCLASS=DBBCOS                                                                                             # TSM devclass name used for DB backup 'backup DB devclass=DBBCOS type=full'
@@ -58,7 +59,7 @@ echo Cloning enckey
 $CMD copy $SRC:$SRC_BUCKET/$INSTANCE_ID/enckey  $DST:/$DST_BUCKET/$INSTANCE_ID/
 
 echo Modifying volhistory
-$CMD cat  $DST:/$DST_BUCKET/$INSTANCE_ID/volhistory.orig/volhistory  | sed -e 's/\"'$SRC_BUCKET'/\"'$DST_BUCKET'/g' | $CMD rcat $DST:/$DST_BUCKET/$INSTANCE_ID/volhistory
+$CMD cat  $DST:/$DST_BUCKET/$INSTANCE_ID/volhistory.orig/volhistory  | sed -e 's/\"'$SRC_BUCKET'/\"'$DST_BUCKET'/g' | $CMD rcat --quiet $DST:/$DST_BUCKET/$INSTANCE_ID/volhistory
 echo Modifying devconfig
 $CMD cat  $DST:/$DST_BUCKET/$INSTANCE_ID/devconfig.orig/devconfig    | sed -e 's/'$SRC_BUCKET'/'$DST_BUCKET'/g'    | $CMD rcat $DST:/$DST_BUCKET/$INSTANCE_ID/devconfig
 
@@ -68,4 +69,4 @@ $CMD cat  $DST:/$DST_BUCKET/$INSTANCE_ID/devconfig.orig/devconfig    | sed -e 's
 # Clones only one last TSM full DB Backup Series
 # Supports full TSM DBB only, no snapshots or incremental backups
 # No synchronization with the actual TSM "backup db" process, requires manual synchronization
-# Target vault retention should be set to 3 days (4 days rotation minus 1 day).
+# Target vault retention should be set to $DBB_RETENTION_INTERVAL-1 days
